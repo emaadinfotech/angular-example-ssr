36a009ec80b124294e5d5a8b74cd513f5ef04468
Tohid Kovadiya <kovadiya.tohid@gmail.com>
2022-01-06 17:31:53 +0530

    cpanel setup ready

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Title, Meta } from '@angular/platform-browser';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
@@ -13,14 +14,46 @@ import { HeroService } from '../hero.service';
 export class HeroDetailComponent implements OnInit {
   @Input() hero!: Hero;
 
+  title = 'Hero Detail - Angular Universal CRUD App';
+
   constructor(
     private route: ActivatedRoute,
     private heroService: HeroService,
-    private location: Location
+    private location: Location,
+    private titleService: Title,
+    private metaTagService: Meta
   ) {}
 
   ngOnInit(): void {
     this.getHero();
+
+    this.titleService.setTitle(this.title);
+    this.metaTagService.updateTag({
+      name: 'description',
+      content: 'Hero Detail - Add song template',
+    });
+
+    this.metaTagService.updateTag({
+      name: 'keywords',
+      content:
+        'Hero Detail - Angular Universal',
+    });
+
+    this.metaTagService.updateTag({
+      name: 'author',
+      content: 'Hero Detail - Tohid M K',
+    });
+
+     this.metaTagService.updateTag({
+       name: 'robots',
+       content: 'Hero Detail - Chrome',
+     });
+
+    this.metaTagService.updateTag({
+      name: 'date',
+      content: 'Hero Detail - 04-01-2022',
+    });
+
   }
 
   getHero(): void {
